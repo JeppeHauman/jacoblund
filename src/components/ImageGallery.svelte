78a4dd748d30df4e4719a66d096948f66f00bdcb
Jeppe Hauman <jeppe_hauman@hotmail.com>
2024-06-20 15:45:33 +0200

    add svelte integration | start image gallery

---
+++ src/components/ImageGallery.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { ImageMetadata } from "astro";
+  export let images: ImageMetadata[] = [];
+
+  let active = 0;
+</script>
+
+<div class="grid grid-cols-5 gap-4 mt-4">
+  {#each images as image, i}
+    <button
+      on:click={() => (active = i)}
+      class={`${active === i ? "border border-black" : "border border-opacity-0 border-black"} transition-all relative group`}
+    >
+      <img src={image.src} alt="doesn mattah" />
+      <div
+        class="absolute top-0 left-0 h-full w-full bg-white bg-opacity-0 group-hover:bg-opacity-20 group-hover:cursor-pointer transition-all"
+      ></div>
+    </button>
+  {/each}
+</div>
